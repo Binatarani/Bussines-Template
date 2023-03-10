c4f22100dc216aba89df61d88adfb397483258f7
Binatarani <binatarani.bd@gmail.com>
2023-03-10 18:55:07 +0600

    section heading

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,6 +1,5 @@
 import Col from "react-bootstrap/Col";
 import "./Service.css";
-// import Button from "../Button/Button";
 import Button2 from "../Button2/Button";
 
 const Service = ({ item }) => {
@@ -8,12 +7,10 @@ const Service = ({ item }) => {
     <Col lg={4}>
       <div className="single-service1">
         <img src={item.img} alt="" className="w-100 mb-5" />
-        <h3>{item.name}</h3>
+        <h2>{item.name}</h2>
         <h5>Price: {item.price}</h5>
         <p>{item.description}</p>
         <Button2 heading="Book Now" />
-        {/* <Button heading="Book Now" /> */}
-        {/* <button className="btn btn-theme">Book Now</button> */}
       </div>
     </Col>
   );
